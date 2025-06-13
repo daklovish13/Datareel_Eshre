17b3900e78e844a6ff02a112176dbd034c16c45b
Lovish <lovish.d@kansoftware.com>
2025-06-13 14:31:39 +0530

    last commit

--- src/app/mobile/page.tsx
+++ src/app/mobile/page.tsx
@@ -4,11 +4,7 @@ import * as React from "react";
 import MultiStepForm from "@/app/mobile/_components/multiform"
 
 
-const steps = [
-  "Select campaign settings",
-  "Create an ad group",
-  "Create an ad",
-];
+
 
 export default function HorizontalLinearStepper() {
   return (
