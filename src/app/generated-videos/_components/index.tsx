b311767fc80591de48cac40c9ccbf84b8ffbf33c
Lovish <lovish.d@kansoftware.com>
2025-06-24 18:16:07 +0530

    "final changes"

--- src/app/generated-videos/_components/index.tsx
+++ src/app/generated-videos/_components/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Header } from "@/components/Header";
 import Image from "next/image";
 import Link from "next/link";
-import { Box, Dialog, DialogContent, Modal, Tooltip } from "@mui/material";
+import {  Dialog, DialogContent,  } from "@mui/material";
 import { ShareSocial } from "react-share-social";
 type VideoData = {
   video_url: string;
